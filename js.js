2b89272416b27757f3fbde3838854e1e022395ab
vienokolsi <bgu203@myy.haaga-helia.fi>
2021-11-23 20:45:35 +0200

    teemun uljas runko

---
+++ js.js
@@ -0,0 +1,9 @@
+function testFunction(dateString){
+    console.log(dateString)
+    // tähän Date.now():lla muuttujaan talteen tämänhetkinen päivä
+    // verrataan parametrin pvm:ää ja nykyistä pvm:ää
+    // jos nykynen pvm on suurempi tai yht kuin parametripvm
+    // saa avata
+
+    //muuten ei saa
+}
