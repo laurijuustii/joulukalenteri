cb45b28741857fd0fd271829c59753ff78613bd7
vienokolsi <bgu203@myy.haaga-helia.fi>
2021-11-23 21:55:26 +0200

    raakileproto a la miro master chef

--- js.js
+++ js.js
@@ -1,9 +1,23 @@
-function testFunction(dateString){
-    console.log(dateString)
+function saakoAvata(dateString) {
+    var luukku = new Date(dateString);
+    var tanaan = new Date();
+    console.log(tanaan.getDate());
+    
+    console.log(luukku);
+    console.log(tanaan);
+    if (luukku >= tanaan) {
+        console.log("ÄLÄ AVAA VIELÄ")
+        document.getElementById(dateString).src ="cat.jpg";
+
+    }
+    else { console.log("Saa avata")};
     // tähän Date.now():lla muuttujaan talteen tämänhetkinen päivä
     // verrataan parametrin pvm:ää ja nykyistä pvm:ää
     // jos nykynen pvm on suurempi tai yht kuin parametripvm
     // saa avata
 
     //muuten ei saa
+}
+function annaQuote(){
+    return //yksi quote listasta quoteja erillisessä tiedostossa
 }
